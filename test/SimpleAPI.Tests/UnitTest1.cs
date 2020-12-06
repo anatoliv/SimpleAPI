dc8dfa70e4cd500ede90af4912c24c149d218177
Anatoli Vishnyakov <anatoli@amnesia.net>
2020-12-06 10:03:54 -0500

    Initial Commit

---
+++ test/SimpleAPI.Tests/UnitTest1.cs
@@ -0,0 +1,24 @@
+using System;
+using Xunit;
+using SimpleAPI.Controllers;
+using Microsoft.Extensions.Logging;
+
+namespace SimpleAPI.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+
+        WeatherForecastController controller = new WeatherForecastController();
+        [Fact]
+        public void GetReturnsCorrectNumber()
+        {
+            var returnValue = controller.Get(1);
+            Assert.Equal("Bracing", returnValue);
+        }
+    }
+}
