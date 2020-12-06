73af383c2e2b940e9fa7c9bb0337162933a9c345
Anatoli Vishnyakov <anatoli@amnesia.net>
2020-12-06 11:11:09 -0500

    Changed the return type of our api/values/n method

--- test/SimpleAPI.Tests/UnitTest1.cs
+++ test/SimpleAPI.Tests/UnitTest1.cs
@@ -18,7 +18,7 @@ namespace SimpleAPI.Tests
         public void GetReturnsCorrectNumber()
         {
             var returnValue = controller.Get(1);
-            Assert.Equal("Bracing", returnValue);
+            Assert.Equal("Bracing xxx", returnValue);
         }
     }
 }
